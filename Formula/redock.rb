24fb23808f4d9c3e6d87ace0db8b7f480117ad68
Kunal Bagaria <kb24x7@gmail.com>
2021-08-08 16:30:59 +0530

    v2.0.0

--- Formula/redock.rb
+++ Formula/redock.rb
@@ -1,9 +1,9 @@
 class Redock < Formula
     desc "A simple CLI tool replace macOS app icons from a png"
     homepage "https://github.com/kb24x7/redock"
-    url "https://github.com/kb24x7/redock/releases/download/v1.2.0/redock.tar.gz"
-    sha256 "9678b5850e5984209e9f1789c3ac2f4facc4663deabaf556648678239886d07c"
-    version "1.2.0"
+    url "https://github.com/kb24x7/redock/releases/download/v2.0.0/redock.tar.gz"
+    sha256 "1a53d3d6a342810d0f578dde2ca91175dae4b6218aec4afac9600e24d864c2cb"
+    version "2.0.0"
 
     def install
         bin.install "redock"
